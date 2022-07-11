95967d77f37537186f70ebae7d2e53d7d6f83e46
J Victor Pessoa <103905578+jotavictorpc@users.noreply.github.com>
2022-07-11 09:58:20 -0300

    Tirando dúvidas sobre a forma de selecionar
    Way of the select. Id, Class. The way that you select in JS, give you problem in your code.

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,21 +1,32 @@
-function carregar() {
-    var msg = window.document.getElementById("msg") // Pegando dado do html
-    var foto = window.document.getElementById("imagem") //Pegando dado do html
-    var data = new Date() //método data 
-    var hora = data.getHours() //Pegando a hora atual do sistema
+function carregar(){
+    var msg = document.getElementById("msg") // Deve ser o id. Se for o className ou query, não vai funcionar
+
+    var img = document.getElementById("imagem") // Deve ser o id. Se for o className ou query, não vai funcionar
+    
+    var data = new Date()
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
 
-    if (hora >= 0 && hora < 12) {
-        //Bom dia 
-        foto.src= "manha-recortada.png"
-        window.document.body.style.background = "rgb(218, 165, 32)"
-    } else if (hora >= 12 && hora <18) {
+    if(hora >=5 && hora <12) {
+         msg.innerHTML = `Bom dia. São ${hora} horas`
+         img.src = "manha-recortada.png"
+         document.body.style.backgroundColor = "rgb(245, 245, 6)"
+    } else if(hora >=12 && hora <18) {
         // Boa tarde
-        foto.src = "tarde-recortada.jpg"
-        window.document.body.style.background = "#9DB2C7"
-    } else {
+        msg.innerHTML = `Boa tarde. São ${hora} horas`
+        img.src = "tarde-recortada.jpg"
+        document.body.style.backgroundColor = "gray"
+    } else if(hora >= 18 && hora < 23.59) {
         // Boa noite
-        foto.src = "noite-recortada.jpg"
-        window.document.body.style.background = "#04244B"
+        msg.innerHTML = `Boa noite. São ${hora} horas`
+        img.src = "noite-recortada.jpg"
+        document.body.style.backgroundColor = " rgba(0, 0, 0, 0.539)"
+    } else if(hora >= 0 && hora <5) {
+        // Boa madrugada
+        msg.innerHTML = `Boa madrugada. São ${hora} horas`
+        img.src = "noitee-recortada.jpg"
+        document.body.style.backgroundColor = "rgba(0, 0, 0, 0.800)"
     }
+
 }
+
